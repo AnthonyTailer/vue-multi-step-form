612bfb0d74c8c5cde57a3c7f7bb69b01f92c8155
Anthony Tailer <anthony.almeida@dbccompany.com.br>
2023-03-20 13:59:51 -0300

    feat: add BaseButton Component

---
+++ client/src/components/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="['baseButton', { outlined }]" v-bind="$attrs">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    outlined: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style scoped>
+  @import '../assets/base.css';
+  .baseButton {
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    background-color: var(--color-button);
+    text-align: center;
+    color: #fff;
+    max-height: 48px;
+  }
+
+  .outlined {
+    background-color: #fff;
+    border: 1px solid var(--color-button);
+    color:  var(--color-button);
+  }
+</style>
