87757a46966ed367f971f8ab0b9657d030ff348e
Anthony Tailer <anthony.almeida@dbccompany.com.br>
2023-03-20 14:01:06 -0300

    feat: add Card Component

---
+++ client/src/components/FormCardWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+<style>
+@import '../assets/base.css';
+
+.card {
+  max-width: 400px;
+  background-color: var(--color-background-card) ;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  padding: 2px 16px;
+}
+</style>
