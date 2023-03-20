56509bdce74138546bc9fc068ccb9bd8e777f77d
Anthony Tailer <anthony.almeida@dbccompany.com.br>
2023-03-20 14:03:32 -0300

    feat: FinalStep Component

---
+++ client/src/views/RegisterForm/FinalStepForm.vue
@@ -0,0 +1,134 @@
+<template>
+  <h1>Etapa 4 de 4</h1>
+  <h1>Revise suas informações</h1>
+
+  <BaseInput
+    name="email"
+    label="Endereço de E-mail"
+    v-model="formValues.email"
+    :error="v$.email.$errors?.[0]?.$message"
+  />
+
+  <div v-if="formValues.personType === 'individual'">
+    <BaseInput 
+      name="name"
+      label="Nome" 
+      v-model="formValues.name"
+      :error="v$.name.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="cpf"
+      label="CPF" 
+      v-model="formValues.cpf"
+      :error="v$.cpf.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="birth"
+      type="date"
+      label="Data de Nascimento" 
+      v-model="formValues.birth"
+      :error="v$.birth.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="phone"
+      label="Telefone" 
+      v-model="formValues.phone"
+      :error="v$.phone.$errors?.[0]?.$message"
+    />
+  </div>
+
+  <div v-else-if="formValues.personType === 'company'">
+    <BaseInput 
+      name="corporateName"
+      label="Razão Social" 
+      v-model="formValues.corporateName"
+      :error="v$.corporateName.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="cnpj"
+      label="CNPJ" 
+      v-model="formValues.cnpj"
+      :error="v$.cnpj.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="openingDate"
+      type="date"
+      label="Data de Abertura" 
+      v-model="formValues.openingDate"
+      :error="v$.openingDate.$errors?.[0]?.$message"
+    />
+
+    <BaseInput 
+      name="phone"
+      label="Telefone" 
+      v-model="formValues.phone"
+      :error="v$.phone.$errors?.[0]?.$message"
+    />
+  </div>
+  
+  <BaseInput
+    name="password"
+    label="Sua senha"
+    v-model="formValues.password"
+    :error="v$.password.$errors?.[0]?.$message"
+  />
+
+  <BaseButton
+    @click="onSubmitStep"
+  >
+    Cadastrar
+  </BaseButton>
+
+  <BaseButton
+    v-if="currentStep > 0"
+    outlined
+    @click="onPreviousStep"
+  >
+    Voltar
+  </BaseButton>
+</template>
+<script>
+import useValidate from "@vuelidate/core";
+import useForm from '../../store/form.js';
+import BaseInput from '../../components/BaseInput.vue';
+import BaseButton from '../../components/BaseButton.vue';
+
+export default {
+  props : {
+    currentStep: { type: Number, default: 0 },
+    formValues: { type: Object, default: {} }
+  },
+  components: { BaseInput, BaseButton },
+  setup(props) {
+    const {
+      getValidationRules
+    } = useForm();
+
+    const rules = getValidationRules(props.formValues);
+    const allRules = rules.value.reduce((prev, current) => ({ ...prev, ...current }), {})
+
+    const v$ = useValidate(allRules, props.formValues);
+
+    return { v$ }
+  },
+  emits: ['submitValidStep', 'submitPreviousStep'],
+  methods: {
+    onSubmitStep ()  {
+       // validate fields
+      this.v$.$validate(); // checks all inputs
+
+      if (!this.v$.$error) {
+        // pass to the next step
+        console.log(`Valid STEP ${this.currentStep}`)
+        console.log('SEND TO BACKEND')
+      }
+    }
+  },
+}
+</script>
+<style></style>
