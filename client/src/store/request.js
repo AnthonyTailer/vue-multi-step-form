1bee43ab2f6a194de453c1c712905600bd183b4e
Anthony Tailer <anthony.almeida@dbccompany.com.br>
2023-03-20 23:01:25 -0300

    fix: request error handling

--- client/src/store/request.js
+++ client/src/store/request.js
@@ -8,24 +8,31 @@ export default function useRequest() {
   });
 
   const postRegistration = async (body, { onSuccess, onError }) => {
+    let response;
+
     try {
       state.isLoading = true;
-      const res = await fetch("http://localhost:3000/registration", {
+      response = await fetch("http://localhost:3000/registration", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
       });
-      state.response = await res.json();
-      console.log("REQUEST COM SUCESSO", { res: state.response });
-      onSuccess?.(state);
     } catch (e) {
-      console.log("REQUEST COM ERRRO", e);
       state.response = e;
       onError?.(state);
     } finally {
       state.isLoading = false;
+    }
+
+    if (response?.ok) {
+      state.response = await response.json();
+      onSuccess?.(state);
+    } else {
+      state.response = await response.json();
+      state.error = true;
+      onError?.(state);
     }
   };
 
